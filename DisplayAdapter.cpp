1ba56219fc7d2b9ba1c013c1468dd74a677039c1
Caspar Kleijne <caspar.kleijne@hyperdata.nl>
2019-01-12 19:30:50 +0100

    Maze Sprites

--- DisplayAdapter.cpp
+++ DisplayAdapter.cpp
@@ -11,10 +11,14 @@ void DisplayAdapter::Clear()
 	DisplayFrame();
 	ClearFrameMemory(0xFF);
 	DisplayFrame();
+	if (Init(lut_partial_update) != 0) {
+		return;
+	}
 }
 
 void DisplayAdapter::Show()
 {
+
 	DisplayFrame();
 }
 
@@ -30,11 +34,9 @@ void DisplayAdapter::Image(const unsigned char * data)
 void DisplayAdapter::Sprite(const unsigned char * data, int index, uint8_t x, uint8_t y, uint8_t size, void transform (unsigned char *))
 {
 
-	unsigned int char_offset = index * size;
-
-	for (int i = 0; i < 72; i++)
+	for (int r = 0; r < 72; r++)
 	{
-		_sprite[i] = ~(pgm_read_byte(&data[i + char_offset]));
+		_sprite[r] = ~(pgm_read_byte(&data[index * 72 + r]));
 	}
 
 	transform(_sprite);
--- Game.cpp
+++ Game.cpp
@@ -15,25 +15,39 @@ void Game::Start()
 
 	//_display->Image(Splash.table);
 
-	_display->Sprite(Sprites.table, 0, 1, 1, 24, SpriteTransform::Default);
-	_display->Sprite(Sprites.table, 0, 1, 3, 24, SpriteTransform::FlipHorizontal);
-	_display->Sprite(Sprites.table, 0, 1, 5, 24, SpriteTransform::FlipVertical);
-	_display->Sprite(Sprites.table, 0, 3, 1, 24, SpriteTransform::Transpose);
 
-	_display->Sprite(Sprites.table, 0, 5, 1, 24, SpriteTransform::Rotate90);
-	_display->Sprite(Sprites.table, 0, 5, 3, 24, SpriteTransform::Rotate180);
-	_display->Sprite(Sprites.table, 0, 5, 5, 24, SpriteTransform::Rotate270);
-
-	Serial.println("start success");
-	_display->Show();
 }
 
 
 
 void Game::UpdateDisplay()
 {
+	delay(100);
+
+	for (int x = 0; x < 5; x++)
+	{
+		_display->Sprite(Sprites.table, x , x % 8, 0, 24, SpriteTransform::Default);
+	}
+
+	for (int x = 0; x < 5; x++)
+	{
+		_display->Sprite(Sprites.table, x, x % 8, 1, 24, SpriteTransform::Rotate90);
+	}
+
+	for (int x = 0; x < 5; x++)
+	{
+		_display->Sprite(Sprites.table, x, x % 8, 2, 24, SpriteTransform::Rotate180);
+	}
+
+	for (int x = 0; x < 5; x++)
+	{
+		_display->Sprite(Sprites.table, x, x % 8, 3, 24, SpriteTransform::Rotate270);
+	}
 
 
+
+	Serial.println("start success");
+	_display->Show();
 }
 
 
@@ -51,19 +65,19 @@ void Game::Run()
 		switch (trigger)
 		{
 		case 1:
-			//player.y--;
+			_display->Sprite(Sprites.table, 0, 5, 1, 24, SpriteTransform::Rotate180);
 
 			break;
 		case 2:
-			//player.x++;
+			_display->Sprite(Sprites.table, 0, 5, 1, 24, SpriteTransform::Rotate270);
 
 			break;
 		case 4:
-			//player.x--;
+			_display->Sprite(Sprites.table, 0, 5, 1, 24, SpriteTransform::Rotate90);
 
 			break;
 		case 8:
-			//player.y++;
+			_display->Sprite(Sprites.table, 0, 5, 1, 24, SpriteTransform::Default);
 
 			break;
 		case 16:
