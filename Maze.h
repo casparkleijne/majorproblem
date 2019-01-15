b867c96cb348e34b26996de996a2f561d06c6e1a
Caspar Kleijne <caspar.kleijne@hyperdata.nl>
2019-01-15 23:46:19 +0100

    some refactoring

--- Maze.h
+++ Maze.h
@@ -1,6 +1,13 @@
 #pragma once
 
+typedef struct _directions
+{
+	int x;
+	int y;
+	unsigned int news; 
+	unsigned int snew;
 
+} Directions;
 
 class Maze
 {
@@ -8,8 +15,12 @@ class Maze
 	int EnQueue(int value);
 	int DeQueue();
 	int GenerateMaze(int pos);
+	int SelectRandomDirection(int pos, Directions * options);
+	int UpdateMap(int pos, Directions options);
 
 public:
+
+
 	void Generate(unsigned char * maze);
 	
 	Maze();
