a5854a9e8e12125d709b28f3f5025ee0996bf18f
Caspar Kleijne <caspar.kleijne@hyperdata.nl>
2019-01-19 12:32:06 +0100

    Reverted

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,9 +1,8 @@
 #include "Maze.h"
 #include <Arduino.h>
 
-
 const uint8_t VISITED = 0x10;
-const Directions _dirs[4] = { { 0, -1, 1, 4 }, { 1, 0, 2, 8 }, { 0, 1, 4, 1 }, { -1, 0, 8, 2 } };
+const int _dir[4][4] = { { 0, -1, 1, 4 }, { 1, 0, 2, 8 }, { 0, 1, 4, 1 }, { -1, 0, 8, 2 } };
 unsigned char _map[64];
 uint8_t _queue[64];
 int _quepos = 0;
@@ -25,30 +24,39 @@ int Maze::DeQueue()
 int Maze::GenerateMaze(int pos)
 {
 	_map[pos] |= VISITED;
-	Directions options[4];
-	int n = SelectRandomDirection(pos, options);
-	return n == -1 ? -1 : UpdateMap(pos, options[n]);
-}
 
-int Maze::SelectRandomDirection(int pos, Directions * options)
-{
+	int x = pos % _width;
+	int y = pos / _width;
+
+	int options[4][4];
 	int optionslength = 0;
+
+
 	for (int i = 0; i < 4; i++)
 	{
-		if ((pos % _width) + _dirs[i].x >= 0 && (pos % _width) + _dirs[i].x < _width && (pos / _width) + _dirs[i].y >= 0 && (pos / _width) + _dirs[i].y < _width && (_map[(pos % _width) + _dirs[i].x + ((pos / _width) + _dirs[i].y) * _width] & VISITED) != VISITED)
+		if (x + _dir[i][0] >= 0 && x + _dir[i][0] < _width && y + _dir[i][1] >= 0 && y + _dir[i][1] < _width && (_map[x + _dir[i][0] + (y + _dir[i][1]) * _width] & VISITED) != VISITED)
 		{
-			options[optionslength++] = _dirs[i];
+			options[optionslength][0] = _dir[i][0];
+			options[optionslength][1] = _dir[i][1];
+			options[optionslength][2] = _dir[i][2];
+			options[optionslength][3] = _dir[i][3];
+			optionslength++;
 		}
 	}
-	return optionslength == 0 ? -1 : rand() % optionslength;
-}
 
-int Maze::UpdateMap(int pos, Directions option)
-{
-	int npos = (pos % _width) + option.x + ((pos / _width) + option.y) * _width;
-	_map[pos] |= option.news;
-	_map[npos] |= option.snew;
-	return npos;
+	if (optionslength == 0)
+	{
+		return -1;
+	}
+
+	int n = rand() % optionslength;
+
+	int newCell = x + options[n][0] + (y + options[n][1]) * _width;
+
+	_map[pos] |= options[n][2];
+	_map[newCell] |= options[n][3];
+	
+	return newCell;
 }
 
 void Maze::Generate(unsigned char * maze)
@@ -57,11 +65,14 @@ void Maze::Generate(unsigned char * maze)
 	{
 		_map[x] = 0x00;
 	}
+
 	int pos = rand() % 64;
 	while (pos != -1)
 	{
+
 		EnQueue(pos);
 		pos = GenerateMaze(pos);
+
 		if (pos == -1)
 		{
 			DeQueue();
--- Maze/Maze.h
+++ Maze/Maze.h
@@ -1,13 +1,6 @@
 #pragma once
 
-typedef struct _directions
-{
-	int x;
-	int y;
-	unsigned int news; 
-	unsigned int snew;
 
-} Directions;
 
 class Maze
 {
@@ -15,12 +8,8 @@ class Maze
 	int EnQueue(int value);
 	int DeQueue();
 	int GenerateMaze(int pos);
-	int SelectRandomDirection(int pos, Directions * options);
-	int UpdateMap(int pos, Directions options);
 
 public:
-
-
 	void Generate(unsigned char * maze);
 	
 	Maze();
